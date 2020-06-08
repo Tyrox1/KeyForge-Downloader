dab6706d166cecce2418cb2749f5ab14a20c1ca8
Tyrox1 <tyrox1@trashmail.com>
2020-06-08 21:43:22 +0200

    Refactoring
    Refactored from urllib to requests and from format-strings to s-Strings.
    Added check for API throtteling and improved Anomaly detection code.
    Also fixed download of Mass Mutation card 001.

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 ### Project Summary
 Tool to download PNG image files of cards from all printed sets.
 
-### Download Instructions
-1. In a code editor of your choice, open the file **downloader.py**.  DO NOT double-click on the file or it will run the script.
-2. On rows 8+ select any expansion that you want to download the cards for (default is all expansions).
-2. On rows 15+ select the languages of cards that you would like to download.  To do so, comment out the languages you don't want. 
-3. Double click to run the file **downloader.py**.
-4. Once the script has run, you will have a new folder *images* that will hold the image files for all cards.
+### Instructions
+1. Make sure, python package `requests` is installed. If not install via `pip install requests` or `pip install -r requirements.txt`.
+2. In a code editor of your choice, open the file **downloader.py**.  DO NOT double-click on the file or it will run the script.
+3. On rows 5+ select any expansion that you want to download the cards for (default is all expansions).
+4. On rows 12+ select the languages of cards that you would like to download.  To do so, comment out the languages you don't want. 
+5. Double click to run the file **downloader.py**.
+6. Once the script has run, you will have a new folder *images* that will hold the image files for all cards.
--- downloader.py
+++ downloader.py
@@ -1,54 +1,48 @@
-import os
-import http.cookiejar
-import urllib.request
-import json
+import os, sys, requests, json, time
 
-SAVE_DIRECTORY = "images/" #The folder where the images get saved
+SAVE_DIRECTORY = "images" #The folder where the images get saved
 #All expansions with their ID
 EXPANSIONS = {
     "341": {"name": "Call of the Archons", "count": 370},
     "435": {"name": "Age of Ascension", "count": 370},
     "452": {"name": "Worlds Collide", "count": 415},
-    "479": {"name": "Mass Mutation", "count": 425}  # 422 cards + 3 gigantic cards
+    "479": {"name": "Mass Mutation", "count": 431} # 421 "normal" cards, 7 versions of Dark Æmber Vault + 3 gigantic cards
     }
 #(Out-)Comment any languages you (don't) want to download
 LANGUAGES = [
-    {"code": "en-us", "folder": "english/"},
-    {"code": "de-de", "folder": "german/"},
-#    {"code": "es-es", "folder": "spanish/"},
-#    {"code": "fr-fr", "folder": "french/"},
-#    {"code": "it-it", "folder": "italian/"},
-#    {"code": "pl-pl", "folder": "polish/"},
-#    {"code": "pt-pt", "folder": "potuguese/"},
-#    {"code": "th-th", "folder": "thai/"},
-#    {"code": "zh-hans", "folder": "chinese-simplified/"},
-#    {"code": "zh-hant", "folder": "chinese-traditional/"}
+    {"code": "en-us", "folder": "english"},
+    {"code": "de-de", "folder": "german"},
+#    {"code": "es-es", "folder": "spanish"},
+#    {"code": "fr-fr", "folder": "french"},
+#    {"code": "it-it", "folder": "italian"},
+#    {"code": "pl-pl", "folder": "polish"},
+#    {"code": "pt-pt", "folder": "potuguese"},
+#    {"code": "th-th", "folder": "thai"},
+#    {"code": "zh-hans", "folder": "chinese-simplified"},
+#    {"code": "zh-hant", "folder": "chinese-traditional"}
     ]
 
 #Create folders for all selected languages and expansions
 for lang in LANGUAGES:
-    if not os.path.exists(SAVE_DIRECTORY + lang["folder"]): #if the folder doesn't exist, create it
-        os.makedirs(SAVE_DIRECTORY + lang["folder"])
+    if not os.path.exists(f"{SAVE_DIRECTORY}/{lang['folder']}"): #if the folder doesn't exist, create it
+        os.makedirs(f"{SAVE_DIRECTORY}/{lang['folder']}")
     for exp_id in EXPANSIONS:
         expansion = EXPANSIONS[exp_id]
-        if not os.path.exists(SAVE_DIRECTORY + lang["folder"] + expansion["name"]):
-            os.makedirs(SAVE_DIRECTORY + lang["folder"] + expansion["name"])
+        if not os.path.exists(f"{SAVE_DIRECTORY}/{lang['folder']}/{expansion['name']}"):
+            os.makedirs(f"{SAVE_DIRECTORY}/{lang['folder']}/{expansion['name']}")
 
-#Initialize URL opener
-cj = http.cookiejar.CookieJar() #for handling all the cookies
-opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-opener.addheaders = [("User-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64;)")]
-
-r = opener.open("https://www.keyforgegame.com/") # to get all the cookies
+session = requests.Session()
+session.get("https://www.keyforgegame.com/")    # to get all the cookies
+session.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64;)"})
 
 #Iterate over all languages. Sadly there is no other way to get the card names in another language.
 for lang in LANGUAGES:
-    opener.addheaders = [("Accept-Language", lang["code"])] #set the language to get translated card names
+    session.headers.update({"Accept-Language" : lang["code"]}) #set the language to get translated card names
 
     #Iterate over all expansion to search for specific decks only
     for exp_id in EXPANSIONS:
         expansion = EXPANSIONS[exp_id]
-        save_dir = SAVE_DIRECTORY + lang["folder"] + expansion["name"]
+        save_dir = f"{SAVE_DIRECTORY}/{lang['folder']}/{expansion['name']}"
 
         #Quick check to see if the cards have already been downloaded
         card_count = len(os.listdir(save_dir))
@@ -56,46 +50,68 @@ for lang in LANGUAGES:
             cards_missing = True
         else:
             cards_missing = False
-            print("==== Download of expansion {} in {} already completed ====".format(expansion["name"], lang["folder"]))
+            print(f"==== Download of expansion {expansion['name']} in {lang['folder']} already completed ====")
 
         #Go over new pages as long as cards are missing
         page_counter = 1
         while cards_missing:
-                r = opener.open("https://www.keyforgegame.com/api/decks/?page={}&expansion={}&links=cards&page_size=100&ordering=-date".format(page_counter, exp_id)) #load a list of decks including links to all the cards
-                decks_raw = r.read()
+            #load a list of decks including links to all the cards
+            r = session.get(f"https://www.keyforgegame.com/api/decks/?page={page_counter}&expansion={exp_id}&links=cards&page_size=50&ordering=-date")
+            
+            if(r.status_code == 429): #Overloaded the API
+                print("[!] Too many requests, waiting 15 seconds.")
+                time.sleep(15)
+                continue #try again
+            
+            decks_raw = r.text
 
-                decks = json.loads(decks_raw)
+            decks = json.loads(decks_raw)
 
-                for card in decks["_linked"]["cards"]:  #Get the info for every card
-                        card_number = str(card["card_number"])
-                        card_title = card["card_title"].replace(" ", "_").replace('"', "'").replace("?", "")
-                        card_image = card["front_image"]
-                        card_expansion = str(card["expansion"])
-                        card_type = card["card_type"]
-                        
-                        if(card_expansion != exp_id):   #Possible legacy card
-                            if(card_expansion in EXPANSIONS):   #Only skip if we know that exp_id, otherwise it's an Anomaly
-                                continue
+            for card in decks["_linked"]["cards"]: #Get the info for every card
+                card_number = str(card["card_number"])
+                card_title = card["card_title"].replace(" ", "_").replace('"', "'").replace("?", "")
+                card_image = card["front_image"]
+                card_expansion = str(card["expansion"])
+                card_type = card["card_type"]
+                card_house = card["house"].replace(" ", "_")
 
-                        if(card_type == "Creature2"):   #Gigantic card second half
-                            card_title += "_gigantic"
+                #Checking for certain unusual card patterns
+                
+                if(card_expansion != exp_id): #Possible legacy/anomaly card
+                    if(card_expansion in EXPANSIONS): #Legacy, skip card
+                        print("  Legacy card, skipping")
+                        continue
+                    elif(card["is_anomaly"] == True): #Anomaly, continue processing
+                        pass
+                    else:
+                        print("[!] An unknown error occured, please report an issue on Github with the following information:")
+                        print("==== Begin Bugreport ====")
+                        print(card)
+                        print("==== End Bugreport ====")
+                        sys.exit(1)
 
-                        filename = save_dir + "/" + card_number + "_" + card_title + ".png"    #local filename to save the image
+                if(card_type == "Creature2"): #Gigantic card second half
+                    card_title += "_gigantic"
 
-                        if not os.path.exists(filename):    #Download file if not already exisiting
-                                print("Downloading " + filename)
-                                r = opener.open(card_image)
-                                with open(filename, "wb") as f:
-                                        f.write(r.read())
-                        else:
-                                print("Already exisiting image, skipping " + filename)
+                if(expansion["name"] == "Mass Mutation" and card_number == "001"): #Dark Æmber Vault
+                    card_title = f"{card_title}_{card_house}"
 
-                #Check if all cards have been downloaded
-                cards_missing = False
-                expansion = EXPANSIONS[exp_id]
-                card_count = len(os.listdir(save_dir))
-                print("==== Downloaded {} out of {} images for expansion {} ====".format(card_count, expansion["count"], expansion["name"]))
-                if card_count < expansion["count"]:
-                        cards_missing = True
+                filename = f"{save_dir}/{card_number}_{card_title}.png" #local filename to save the image
 
-                page_counter += 1
+                if not os.path.exists(filename): #Download file if not already exisiting
+                        print(f"  Downloading {filename}")
+                        ri = session.get(card_image)
+                        with open(filename, 'wb') as f:
+                                f.write(ri.content)
+                else:
+                        print(f"  Already exisiting image, skipping {filename}")
+
+            #Check if all cards have been downloaded
+            cards_missing = False
+            expansion = EXPANSIONS[exp_id]
+            card_count = len(os.listdir(save_dir))
+            print(f"==== Downloaded {card_count} out of {expansion['count']} images for expansion {expansion['name']} ====")
+            if card_count < expansion["count"]:
+                cards_missing = True
+
+            page_counter += 1
